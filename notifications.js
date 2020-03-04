f3439f5b224d07aed14e01e57daa117c5b8df220
clayton <me@clayton.cc>
2020-03-03 21:00:12 -0800

    Initial commit

---
+++ notifications.js
@@ -0,0 +1,17 @@
+const request = require('request');
+const config = require('./config.json');
+
+function sendNotification(content) {
+    return request({
+        uri: 'https://api.pushed.co/1/push',
+        method: 'POST',
+        form: {
+            app_key: config.notification_key,
+            app_secret: config.notification_secret,
+            content,
+            target_type: 'app'
+        }
+    });
+}
+
+module.exports = { sendNotification };
